fcd19ff10db3dfc382c1b9b1d01099c96c391120
Curt <clrozeboom@gmail.com>
2025-08-12 18:59:33 -0400

    Use YAMS library

--- Example/src/main/java/frc/robot/BuildConstants.java
+++ Example/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Example";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 41;
-  public static final String GIT_SHA = "bd47c3257cf6c6c9832311205fc17e2bc0d90d63";
-  public static final String GIT_DATE = "2025-07-20 14:46:48 EDT";
+  public static final int GIT_REVISION = 42;
+  public static final String GIT_SHA = "efa4420d2bda2c318413389a60565c966170d8e2";
+  public static final String GIT_DATE = "2025-07-20 15:39:28 EDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2025-07-20 15:33:23 EDT";
-  public static final long BUILD_UNIX_TIME = 1753040003241L;
+  public static final String BUILD_DATE = "2025-08-12 18:58:07 EDT";
+  public static final long BUILD_UNIX_TIME = 1755039487646L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
